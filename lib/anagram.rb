15a8c8491fb717aca0d7ed4d7eb0c61be0d140ca
Robert Andrew Lin <github email address>
2016-09-29 14:59:56 -0400

    Finished

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_anagram_array)
+    word_array = @word.split("").sort!
+    possible_anagram_array.select {|anagram_word| anagram_word.split("").sort! == word_array}
+  end
+
+end
